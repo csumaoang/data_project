84fa5e43320c8f0cf1bedd66dec2bcce18b37455
csumaoang <csumaoang@academic.rrc.ca>
2024-07-10 11:26:00 -0500

    Changed games linking in character, boss, and item show pages to work for multiple games

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'about/index'
-  resources :games, only: [:index, :show]
+  root 'home#index'
+  
+  get 'search', to: 'search#index'
+  get 'about', to: 'about#index'
+
+  resources :games, only: [:index, :show] do
+    resources :characters, only: [:index, :show]
+    resources :bosses, only: [:index, :show]
+    resources :items, only: [:index, :show]
+  end
+
   resources :characters, only: [:index, :show]
   resources :bosses, only: [:index, :show]
   resources :items, only: [:index, :show]
-
-  root 'home#index'
-  get 'search', to: 'search#index'
-  get 'about', to: 'about#index'
 end
